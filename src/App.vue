631e4137510d5a6a8b513105bdfe318d18127f95
Phu-Hiep DUONG <duongphuhiep@gmail.com>
2017-03-16 23:42:06 +0100

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-    <Hero />
+     <h1>Hello App!</h1>
+        <p>
+            <router-link to="/foo">Go to Foo</router-link>
+            <router-link to="/bar">Go to Bar</router-link>
+        </p>
+        <router-view></router-view>
   </div>
 </template>
 
 <script>
+	
 import Hero from './home/Hero.vue';
+import Vue from 'vue';
+
 export default {
   name: 'app',
   components: {Hero},
--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router';
+
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+	{ path: '/foo', component: Foo },
+	{ path: '/bar', component: Bar }
+]
+
+const app = new Vue({
+	router
+}).$mount('#app')
+
+
+const router = new VueRouter({
+	routes
+})
+
+
+Vue.use(VueRouter);
+
+
 new Vue({
-  el: '#app',
-  render: h => h(App)
+	router,
+	el: '#app',
+	render: h => h(App)
 })
